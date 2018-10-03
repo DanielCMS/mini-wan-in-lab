f94203248cdd127b0a88a0fd663d57d567a0d7cd
Linqi <linqi@waltznetworks.com>
2018-10-02 23:15:05 -0700

    Implement host panel

--- src/app/host-panel/host-panel.component.ts
+++ src/app/host-panel/host-panel.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Host } from '../network-devices';
 import { Vector } from '../vector';
+
+const X_OFFSET = 100;
 
 @Component({
   selector: 'app-host-panel',
   templateUrl: './host-panel.component.html',
-  styleUrls: ['./host-panel.component.css']
+  styleUrls: ['./host-panel.component.scss']
 })
 export class HostPanelComponent implements OnInit {
 
   @Input() model: Host;
   @Input() canvasOffset: Vector;
+  @Output() close = new EventEmitter<void>();
+
+  private anchor: Vector;
 
   constructor() { }
 
   ngOnInit() {
+    this.anchor = {
+      x: this.model.position.x + this.canvasOffset.x + X_OFFSET,
+      y: this.model.position.y + this.canvasOffset.y
+    };
+  }
+
+  private closePanel(): void {
+    this.close.emit();
   }
 
 }
