2cd2897a1944415986458e2465e0085e2695dee3
Linqi <linqi@waltznetworks.com>
2018-10-03 14:29:56 -0700

    Implement the feedback system

---
+++ src/app/feedback.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+export enum MessageType {
+  INFO,
+  WARNING,
+  ERROR
+}
+
+export class Message {
+  constructor(public text: string, public type: MessageType) {}
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeedbackService {
+
+  private messages: Message[] = [];
+
+  constructor() { }
+
+  public sendInfo(msg: string): void {
+    this.messages.push(new Message(msg, MessageType.INFO));
+  }
+
+  public sendWarning(msg: string): void {
+    this.messages.push(new Message(msg, MessageType.WARNING));
+  }
+
+  public sendError(msg: string): void {
+    this.messages.push(new Message(msg, MessageType.ERROR));
+  }
+
+  public removeMsg(msg: Message): void {
+    this.messages = this.messages.filter(_m => _m !== msg);
+  }
+
+}
