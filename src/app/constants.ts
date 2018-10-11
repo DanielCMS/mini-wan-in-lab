ed9d5e54f55ba7c35a03eb824d9245d69e2163b8
Linqi <linqi@waltznetworks.com>
2018-10-11 16:04:09 -0700

    A few style updates

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,14 +1,14 @@
 export const TIME_SLOWDOWN = 10;
 export const BROADCAST_IP = "0.0.0.0";
-export const OSPF_SIZE = 1024;			// 1KB
-export const PKT_SIZE = 1024;			// 1KB
-export const HEADER_SIZE = 20;			// 20B
-export const CTL_SIZE = 64;				// 64B
+export const OSPF_SIZE = 1024;  // 1KB
+export const PKT_SIZE = 1024;   // 1KB
+export const HEADER_SIZE = 20;  // 20B
+export const CTL_SIZE = 64;  // 64B
 export const PAYLOAD_SIZE = PKT_SIZE - HEADER_SIZE;
 export const IPv4 = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/;
 
 export const RWND_INIT = Number.MAX_VALUE;
 export const SSTHRESH_INIT = Number.MAX_VALUE;
 
-export const BETA = 2;					// RTO = BETA * RTT
-export const MEGA = 1024 * 1024;		// 1 MB = 1024 * 1024 Byte
+export const BETA = 2; // RTO = BETA * RTT
+export const MEGA = 1024 * 1024; // 1 MB = 1024 * 1024 Byte
--- src/app/flow.ts
+++ src/app/flow.ts
@@ -1,7 +1,9 @@
-import {Host, Packet, PacketType} from './network-devices'
-import { PKT_SIZE, HEADER_SIZE, PAYLOAD_SIZE, CTL_SIZE, RWND_INIT, SSTHRESH_INIT, BETA, MEGA } from './constants';
+import { Host, Packet, PacketType } from './network-devices'
+import { PKT_SIZE, HEADER_SIZE, PAYLOAD_SIZE, CTL_SIZE,
+  RWND_INIT, SSTHRESH_INIT, BETA, MEGA } from './constants';
 
 export class Flow {
+
   public flowId: number;
   public flowSource: string;
   public flowDestination: string;
@@ -24,28 +26,36 @@ export class Flow {
   public maxAckDup: number = 0;
   public timeSYN: number;
   public timeSYNACK: number;
+
   constructor(public id: number, public src: Host, public destIP: string, public data: number, public time: number) {
     this.flowId = id;
     this.flowSource = src.getIp();
     this.flowDestination = destIP + "/24";
     this.sendingHost = src;
-    this.dataAmount = data;      // in MB
-    data = data * MEGA;          // in B
+    this.dataAmount = data; // in MB
+    data = data * MEGA;  // in Byte
     this.waitingTime = time;
-    this.initTime = new Date().getTime();
+    this.initTime = Date.now();
     this.algorithm = AlgType[src.algorithm];
+
     let seqNum = 1;
+
     this.packetList = [];
-    while (data > 0){
+
+    while (data > 0) {
       let pkt_size = Math.min(data, PAYLOAD_SIZE);
+
       this.packetList.push(new Packet(this.flowId, src.getIp(), destIP + "/24", PacketType.Payload, seqNum, HEADER_SIZE + pkt_size))
       seqNum++;
       data = data - pkt_size;
     }
+
     this.flowStatus = FlowStatus.Waiting;
-    for(var n = 0; n < time; n++ ){
-      setTimeout(()=>this.countDown(), (n+1)*1000);
+
+    for (let n = 0; n < time; n++) {
+      setTimeout(() => this.countDown(), (n + 1) * 1000);
     }
+
     setTimeout(()=>this.handShake(), time * 1000);
   }
 
@@ -57,20 +67,21 @@ export class Flow {
     this.flowStatus = FlowStatus.HandShake;
     this.packetSYN = new Packet(this.flowId, this.flowSource, this.flowDestination, PacketType.Syn, 0, CTL_SIZE);
     this.timeSYN = Date.now();
-    setTimeout(()=>this.sendingHost.sendPacket(this.packetSYN));
+    setTimeout(() => this.sendingHost.sendPacket(this.packetSYN));
   }
 
   public onReceive(packet: Packet): void {
     if (packet.type === PacketType.SynAck) {
       this.packetACK = new Packet(this.flowId, this.flowSource, this.flowDestination, PacketType.Ack, 1, CTL_SIZE);
-      setTimeout(()=>this.sendingHost.sendPacket(this.packetACK));
+      setTimeout(() => this.sendingHost.sendPacket(this.packetACK));
       //this.RTT = packet.getTransTime() + this.packetSYN.getTransTime();
       //this.RTO = BETA * this.RTT;
       //console.log('RTO', this.RTO);
       this.timeSYNACK = Date.now();
-      console.log('RTT', this.timeSYNACK-this.timeSYN);
-      this.RTT = this.timeSYNACK-this.timeSYN;
+      console.log('RTT', this.timeSYNACK - this.timeSYN);
+      this.RTT = this.timeSYNACK - this.timeSYN;
       this.RTO = BETA * this.RTT;
+
       switch(this.algorithm) {
         case AlgType.Reno:
           this.sendPackets_Reno();
@@ -80,8 +91,10 @@ export class Flow {
         default:
           break;
       }
+
       return;
     }
+
     if (packet.type === PacketType.Ack) {
       switch(this.algorithm) {
         case AlgType.Reno:
@@ -170,17 +183,15 @@ export class Flow {
     }
   }
 //-------- Above is the code for TCP Reno --------
-
-
-
 }  
 
 
 export class FlowReceived {
-  public flowId: number;          // id of the received flow
-  public rwnd: number;            // Receive window size
-  public pktRecieved: number[];   // seq numbers of received packets
-  public nextAck: number;         // seq number of the the next Ack packet
+  public flowId: number; // id of the received flow
+  public rwnd: number; // Receive window size
+  public pktRecieved: number[]; // seq numbers of received packets
+  public nextAck: number; // seq number of the the next Ack packet
+
   constructor(public id: number) {
     this.flowId = id;
     this.rwnd = RWND_INIT;
@@ -212,8 +223,8 @@ export enum AlgType {
 export enum FlowStatus {
   Waiting,
   Complete,
-  SS,    // slow start
-  CA,    // comgestion avoidance
-  FRFR,  // fast retansmit/fast recovery
+  SS, // slow start
+  CA, // comgestion avoidance
+  FRFR,// fast retansmit/fast recovery
   HandShake
 }
